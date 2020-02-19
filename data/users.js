59cc7d3fadbdb927a288a40888148ca43b57c41a
Maxim <maxim.gordiyenko@gmail.com>
2020-02-19 20:56:34 +0200

    added more data

--- data/users.js
+++ data/users.js
@@ -10,21 +10,40 @@ let users = [
         "page_id": 1,
         "users": [
             {"id": 2, "name": "Kate", "surname": "Winslet"},
-            {"id": 3, "name": "Brandon", "surname": "Walsh"}
+            {"id": 3, "name": "Brandon", "surname": "Walsh"},
+            {"id": 5, "name": "Mason", "surname": "Card"}
         ],
     },
     {
         "page_id": 2,
         "users": [
             {"id": 4, "name": "Rebeca", "surname": "Cocney"},
-            {"id": 5, "name": "Mason", "surname": "Card"}
+            {"id": 6, "name": "Jesy", "surname": "Pinkmen"},
+            {"id": 7, "name": "Wolter", "surname": "White"},
+            {"id": 8, "name": "Colly", "surname": "Bracko"},
         ],
     },
     {
         "page_id": 3,
         "users": [
-            {"id": 6, "name": "Jesy", "surname": "Pinkmen"},
-            {"id": 7, "name": "Wolter", "surname": "White"}
+            {"id": 9, "name": "Rebeca", "surname": "Cocney"},
+        ],
+    },
+    {
+        "page_id": 4,
+        "users": [
+            {"id": 10, "name": "Jesy", "surname": "Pinkmen"},
+            {"id": 11, "name": "Wolter", "surname": "White"},
+            {"id": 12, "name": "Colly", "surname": "Bracko"},
+        ],
+    },
+    {
+        "page_id": 5,
+        "users": [
+            {"id": 13, "name": "Matt", "surname": "Hosly"},
+            {"id": 14, "name": "Colin", "surname": "Farel"},
+            {"id": 15, "name": "Win", "surname": "Disel"},
+            {"id": 16, "name": "Sui", "surname": "Mudel"},
         ],
     },
 ];
