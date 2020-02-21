6e75d64c7e5bf3f1ea76d4315744a87a1b25f769
Maksym Hordiienko1 <maksym_hordiienko1@epam.com>
2020-02-21 18:18:34 +0200

    fixed error status

--- routers/users.js
+++ routers/users.js
@@ -10,8 +10,9 @@ router.get('/', (req, res) => {
 // method: GET, path: /pages, page index: /7
 router.get('/:id', (req, res) => {
     let page_num_index = pages.map(i => i.page_id).indexOf(parseInt(req.params.id));
+    console.log(typeof page_num_index);
     if (page_num_index >= pages.length || page_num_index < 0) {
-        res.status(404).send('what???');
+        res.status(404).send('[]');
     } else {
         res.status(200).send(pages[page_num_index]);
     }
