7941f017e9785d9d36e728b51a9130da44e8f1e6
Maxim <maxim.gordiyenko@gmail.com>
2020-02-29 12:25:51 +0200

    Made independent req.id

--- routers/users.js
+++ routers/users.js
@@ -4,7 +4,8 @@ const data = require("../data/data");
 
 // method: GET, path: /users, id: /7
 users_route.get('/:id', (req, res) => {
-    let getUserById = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === parseInt(req.params.id)), []);
+    let req_id = parseInt(req.params.id);
+    let getUserById = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
     res.status(200).send(getUserById);
 });
 
