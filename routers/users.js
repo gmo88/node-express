6cf88ae07298e8a953fb3fe994c6f4a6dbc301f3
gmo88 <maxim.gordiyenko@gmail.com>
2020-03-02 16:05:24 +0200

    added condition for users id

--- routers/users.js
+++ routers/users.js
@@ -5,8 +5,14 @@ const data = require("../data/data");
 // method: GET, path: /users, id: /7
 users_route.get('/:id', (req, res) => {
     let req_id = parseInt(req.params.id);
-    let getUserById = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
-    res.status(200).send(getUserById);
+    let users_data = data.map(e => e.users);
+    let users_length = users_data.flat().length;
+    if (req_id <= users_length && users_length >= 0) {
+        let getUserById = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
+        res.status(200).send(getUserById);
+    } else {
+        res.status(404).send('{}');
+    }
 });
 
 module.exports = users_route;
