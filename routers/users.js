b1b3e3186a4e1d5bbd33dc23b8b8ba2cd689210e
gmo88 <maxim.gordiyenko@gmail.com>
2020-03-05 13:45:58 +0200

    rename variable to properly name and fix condition

--- routers/users.js
+++ routers/users.js
@@ -5,11 +5,9 @@ const data = require("../data/data");
 // method: GET, path: /users, id: /7
 users_route.get('/:id', (req, res) => {
     let req_id = parseInt(req.params.id);
-    let users_data = data.map(e => e.users);
-    let users_length = users_data.flat().length;
-    if (req_id <= users_length && users_length >= 0) {
-        let getUserById = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
-        res.status(200).send(getUserById);
+    let userObject = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
+    if (Array.isArray(userObject) && userObject.length) {
+        res.status(200).send(userObject);
     } else {
         res.status(404).send('{}');
     }
