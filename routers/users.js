bce09c89b4f4e0cf6a9224c067c0b6cd25d9c78f
gmo88 <maxim.gordiyenko@gmail.com>
2020-03-06 21:21:15 +0200

    repalce reduce on for loop

--- routers/users.js
+++ routers/users.js
@@ -5,11 +5,20 @@ const data = require("../data/data");
 // method: GET, path: /users, id: /7
 users_route.get('/:id', (req, res) => {
     let req_id = parseInt(req.params.id);
-    let userObject = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
-    if (Array.isArray(userObject) && userObject.length) {
-        res.status(200).send(userObject);
-    } else {
+    console.log(req_id);
+    // let userObject = data.reduce((acc, el) => [...acc, ...el.users].filter(e => e.id === req_id), []);
+    let userObject;
+    for (let i = 0; i < data.length; i++) {
+        console.log("data[i].users", data[i].users);
+        for (let j = 0; j < data[i].users.length; j++) {
+            userObject = data[i].users[j];
+            console.log("userObject.id:", userObject.id);
+        }
+    }
+    if (userObject.id === req_id) {
         res.status(404).send('{}');
+    } else {
+        res.status(200).send(userObject);
     }
 });
 
