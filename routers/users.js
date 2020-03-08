ab46377f0e0617cbceabc2e14391259b78dc13af
Maxim <maxim.gordiyenko@gmail.com>
2020-03-08 18:51:06 +0200

    issue-7 shortened one for loop with array .find()

--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const users_route = express.Router();
 const data = require("../data/data");
-const { performance } = require('perf_hooks');
+const {performance} = require('perf_hooks');
 
 // method: GET, path: /users, id: /7
 users_route.get('/:id', (req, res) => {
@@ -9,12 +9,7 @@ users_route.get('/:id', (req, res) => {
     let req_id = parseInt(req.params.id);
     let userObject = null;
     for (let i = 0; i < data.length && userObject == null; i++) {
-        for (let j = 0; j < data[i].users.length; j++) {
-            if (data[i].users[j].id === req_id) {
-                userObject = data[i].users[j];
-                break;
-            }
-        }
+        userObject = data[i].users.find(el => el.id === req_id);
     }
     const endTime = performance.now();
     console.log("Execution time: %dms", endTime - beginTime);
