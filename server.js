4876404c92bf69faa3b68c8e135b529201ab3b7c
Maxim <maxim.gordiyenko@gmail.com>
2020-03-09 22:21:38 +0200

    issue-12 make fatch withdisplay data

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser');
 const pages = require('./routers/pages');
 const users = require('./routers/users');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(express.static('public')); // relative path of client-side code
+app.get('/', (req, res) => {
+    res.sendFile('index1.html', {root: __dirname});
+});
 
 app.use('/pages', pages);
 app.use('/users', users);
